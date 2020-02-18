0cae0576eddcfe902d52d5b0da7aa054e840d9d1
Dakota Willingham <kota@vaga.tk>
2020-02-18 22:51:41 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,14 @@
 def square_array(numbers)
   counter = 0
   while counter < numbers.length do
+<<<<<<< HEAD
     numbers[counter] **= 2
     counter += 1
   end
   numbers
+=======
+    numbers[counter] = numbers[counter] ** 2)
+    counter += 1
+  end
+>>>>>>> fbb7ea8fb9f8a3d1529ff05b41c1d159e49be032
 end
